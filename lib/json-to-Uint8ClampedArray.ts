8bab835d80ea2f7af50e05cb41667d079148eb14
SignpostMarv <me@signpostmarv.name>
2024-06-15 23:20:59 +0000

    adjusting indentation

--- lib/json-to-Uint8ClampedArray.ts
+++ lib/json-to-Uint8ClampedArray.ts
@@ -33,20 +33,20 @@ export async function json_to_Uint8ClampedArray(): Promise<Uint8ClampedArray|Buf
 			`mDataWidth changed!`
 		))
 
-	const data = new Uint8ClampedArray(mDataWidth * mDataWidth * 4);
+		const data = new Uint8ClampedArray(mDataWidth * mDataWidth * 4);
 
-	for (let index=0; index < mAreaData.length; ++index) {
-		if (mAreaData[index] >= mColorPalette.length) {
-			throw new Error(`No palette at index ${index}`);
+		for (let index=0; index < mAreaData.length; ++index) {
+			if (mAreaData[index] >= mColorPalette.length) {
+				throw new Error(`No palette at index ${index}`);
+			}
+			const {R, G, B, A} = mColorPalette[mAreaData[index]];
+
+			data.set([R, G, B, A], index * 4);
 		}
-		const {R, G, B, A} = mColorPalette[mAreaData[index]];
 
-		data.set([R, G, B, A], index * 4);
-	}
+		await writeFile(here_is_one_we_made_earlier, data);
 
-	await writeFile(here_is_one_we_made_earlier, data);
-
-	return data;
+		return data;
 	} else {
 		return readFile(here_is_one_we_made_earlier);
 	}
